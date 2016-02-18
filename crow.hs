a54bdf2f8e11d455740acdf64dfa7842a979e3f1
osfameron <hakim.cassimally@gmail.com>
2016-02-18 23:26:09 +0000

    Parse whole crow grid

--- crow.hs
+++ crow.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Function
+import qualified Data.Map as M
 
 class (Show a) => Stringify a where
     stringify :: a -> String
@@ -13,14 +14,28 @@ data Cell = Black | White (Maybe Char)
 data GridCell = GridCell Coord Cell
     deriving Show
 
+type Grid = [[GridCell]]
+
 data Run = Run Dir [GridCell]
     deriving Show
 
 data Light = Light Int Run
     deriving Show
 
+data Crow = Crow Grid (M.Map Coord [Light])
+    deriving Show
+
 instance Stringify Light where
     stringify (Light i (Run d cs)) = intercalate " " [(show i), (show d), "(" ++ (show . length $ cs) ++ ")"]
+
+data Clue = Clue String [Light] [Int]
+    deriving Show
+
+instance Stringify Clue where
+    stringify (Clue s l e) = 
+        let l' = intercalate ", " $ map (\(Light i (Run d _)) -> (show i) ++ " " ++ (show d)) l
+            e' = "(" ++ (intercalate "," $ map show e) ++ ")"
+        in intercalate " " [l' ++ ".", s, e']
 
 type Coord = (Int,Int)
 type Length = Int 
@@ -49,7 +64,7 @@ parseCell c = White $ Just c
 isWhite (GridCell _ Black) = False
 isWhite _ = True
 
-parseGrid :: [ String ] -> [ [ GridCell ] ]
+parseGrid :: [ String ] -> Grid
 parseGrid lines =
     let rows = zipWith makeRow [0..] $ lines
         makeRow rownum row = 
@@ -65,7 +80,8 @@ getRuns dir line =
 
 headPos (Run _ ( (GridCell pos _) : _ )) = pos
 
-getAllLights grid =
+getLights :: Grid -> [Light]
+getLights grid =
     let acrosses = concatMap (getRuns Across) $ parseGrid cw
         downs = concatMap (getRuns Down) . transpose $ parseGrid cw
         all = sortBy (compare `on` headPos) $ acrosses ++ downs
@@ -73,4 +89,20 @@ getAllLights grid =
         makeLightNs g n = map (Light n) g
         numbered = zipWith makeLightNs grouped [1..]
     in concat numbered
+
+getGridCell2LightMap ls =
+    let coord2l l@(Light _ (Run _ gcs)) =
+            let coords = map (\(GridCell pos _) -> pos) gcs
+            in zip coords $ repeat [l]
+        ls' = concatMap coord2l ls
+    in M.fromListWith (++) ls'
+
+parseGrid2Crow lines =
+    let grid = parseGrid lines
+        lights = getLights grid
+        lm = getGridCell2LightMap lights
+    in Crow grid lm
+
+instance Stringify Crow where
+    stringify (Crow grid lm) = "TODO"
 
